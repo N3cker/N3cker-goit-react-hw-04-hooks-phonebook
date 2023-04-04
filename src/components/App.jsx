894e725bc54eb2a30dd9fb1e57e5629353552f49
Mateusz Piasecki <piaszuu@gmail.com>
2023-04-04 15:04:01 +0200

    update files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,19 @@
+import React, { useState } from 'react';
+import ContactForm from './ContactForm';
+import Filter from './Filter';
+import ContactList from './ContactList';
+
 export const App = () => {
+  const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useState([]);
+
+  React.useEffect(() => {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      setContacts(JSON.parse(storedContacts));
+    }
+  }, []);
+
   return (
     <div
       style={{
@@ -6,11 +21,20 @@ export const App = () => {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
+        flexDirection: 'column',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <h1>Phonebook</h1>
+      <ContactForm contacts={contacts} setContacts={setContacts} />
+      <h2>Contacts</h2>
+      <Filter filter={filter} setFilter={setFilter} />
+      <ContactList
+        contacts={contacts}
+        filter={filter}
+        setContacts={setContacts}
+      />
     </div>
   );
 };
